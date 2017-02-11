59bc543351d7b250a00a34b3b88cf3dd1ce20ae1
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-11 11:19:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-/go/bin/gost -L ss://chacha20:$PASSWORD@:8080  -F wss://$REMOTEADDR:443 -logtostderr -v=3
+/go/bin/gost -L ss://chacha20:$PASSWORD@:8080  -F $MODE://$REMOTEADDR:443 -logtostderr -v=3
